072ee1d87ae3bd36d7f5d993ed916b0524f87fe2
Prateek Gupta <114001327+prateek3103@users.noreply.github.com>
2025-02-28 01:00:42 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ export default function Custom404() {
     return (
       <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
         <h1 className="text-6xl font-bold text-gray-900 dark:text-white">404</h1>
-        <p className="text-2xl text-gray-700 dark:text-gray-300">Oops&apos This page doesn't exist.</p>
+        <p className="text-2xl text-gray-700 dark:text-gray-300">Oops! This page doesn&apost exist.</p>
         <img
           src="https://i.imgflip.com/7w7k5s.jpg"
           alt="Funny Meme"
